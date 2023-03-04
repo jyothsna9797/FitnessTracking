a4648de14e4ecbea65a798e1293e6244f4894ca2
Uday1720 <udaykiranr03@gmail.com>
2023-03-03 21:28:47 -0600

    created fitness app launch page

---
+++ Workout/app/src/main/java/com/workout/scout/LoginsingupActivity.java
@@ -0,0 +1,12 @@
+package com.workout.scout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginsingupActivity extends Activity {
+    protected void onCreate(Bundle _savedInstanceState) {
+        super.onCreate(_savedInstanceState);
+        setContentView(R.layout.loginsingup);
+    }
+
+}
